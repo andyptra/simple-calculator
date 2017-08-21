1d366a6d7f73069790a5aa73d063a443c4c91de0
andyptra <andygrap@gmail.com>
2017-08-21 07:33:32 +0700

    final version

--- simple calculator/ViewController.swift
+++ simple calculator/ViewController.swift
@@ -10,16 +10,67 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var firstnumbertxt: UITextField!
+    @IBOutlet weak var seconnumbertxt: UITextField!
+    @IBOutlet weak var result: UILabel!
+    
+    var results = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        result.text = "Result is : "
     }
 
 
+
+
+    @IBAction func plusbtn(_ sender: Any) {
+        
+        if let firstnumber = Int(firstnumbertxt.text!){
+            if let secondnumber = Int(seconnumbertxt.text!){
+                 results = firstnumber + secondnumber
+                result.text=" result is \(String(results))"
+            }
+        }
+      
+        
+    }
+    
+    @IBAction func minusbtn(_ sender: Any) {
+        if let firstnumber = Int(firstnumbertxt.text!){
+            if let secondnumber = Int(seconnumbertxt.text!){
+                results = firstnumber - secondnumber
+                result.text=" result is \(String(results))"
+            }
+        }
+    }
+    
+    @IBAction func dividerbtn(_ sender: Any) {
+        if let firstnumber = Int(firstnumbertxt.text!){
+            if let secondnumber = Int(seconnumbertxt.text!){
+                results = firstnumber / secondnumber
+                result.text=" result is \(String(results))"
+            }
+        }
+    }
+    
+    @IBAction func mulbtn(_ sender: Any) {
+        if let firstnumber = Int(firstnumbertxt.text!){
+            if let secondnumber = Int(seconnumbertxt.text!){
+                results = firstnumber * secondnumber
+                result.text=" result is \(String(results))"
+            }
+        }    }
+    
+    @IBAction func reminderbtn(_ sender: Any) {
+        if let firstnumber = Int(firstnumbertxt.text!){
+            if let secondnumber = Int(seconnumbertxt.text!){
+                results = firstnumber % secondnumber
+                result.text=" result is \(String(results))"
+            }
+        }
+    }
+    
 }
 
